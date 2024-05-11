b82da3a39013f33ae6d3f43396e0ab0be7ff0571
Dj Q-Sa <147480541+zoomser@users.noreply.github.com>
2024-05-11 22:05:52 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
 let burger=document.querySelector('.burger')
 let menu=document.querySelector('.nav_ul')
 burger.addEventListener("click",function(){
-menu.classList.toggle("show")
-burger.classList.toggle("active_burger");
+    menu.classList.toggle("show")
+    burger.classList.toggle("active_burger");
 })
 window.addEventListener('click', function (e) {
     if (!burger.contains(e.target) && !menu.contains(e.target)) {
         burger.classList.remove('active_burger');
         menu.classList.remove('show');
-    }
+        }
 });
+
